631e523d9f8e96e85a3d7c26f5f0add0e17e4083
AlnoSabetha09 <107601359+AlnoSabetha09@users.noreply.github.com>
2024-09-26 22:05:43 +0700

    first commit:selesai stack layout

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjekProgmob
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
